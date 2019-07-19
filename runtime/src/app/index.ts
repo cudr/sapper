f9916f1e0bcedcc2bb426417238ccef388d87ba9
cudr <codefaster@yandex.ru>
2019-07-19 16:26:48 +0300

    export scroll history

--- runtime/src/app/index.ts
+++ runtime/src/app/index.ts
@@ -3,6 +3,7 @@ import { CONTEXT_KEY } from '@sapper/internal/shared';
 
 export const stores = () => getContext(CONTEXT_KEY);
 
+export { scroll_history, cid } from './app';
 export { default as start } from './start/index';
 export { default as goto } from './goto/index';
 export { default as prefetch } from './prefetch/index';
